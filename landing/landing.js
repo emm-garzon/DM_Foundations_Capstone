84d3c4c40e93acfc92a919c944e76e49278495f4
Emmanuel Garzon <eggarzon@gmail.com>
2022-05-06 12:16:01 -0700

    css updates, logo change

--- landing/landing.js
+++ landing/landing.js
@@ -1,9 +1,11 @@
 const userInfo = document.getElementById("user-info");
 let famNameInput = document.getElementById("famName");
+let userInput = document.querySelector("input");
 
 userInfo.addEventListener("submit", (event) => {
   event.preventDefault();
 
   window.localStorage.setItem("familyName", famNameInput.value);
   window.location.href = "../public/index.html";
+  userInput.value = "";
 });
